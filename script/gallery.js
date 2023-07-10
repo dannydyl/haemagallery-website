d5c157cda152943480bd4d4e4d4773643c28de41
dannydyl <dannydyl8@gmail.com>
2023-07-10 15:45:06 -0400

    Xscroll done. mouse sync not yet

--- script/gallery.js
+++ script/gallery.js
@@ -10,8 +10,9 @@ const handleOnDown = e => {
 //클릭을 땔때 다시 초기화
 const handleOnUp = () => {
   track.dataset.mouseDownAt = "0";  
-  track.dataset.prevPercentage = track.dataset.percentage;
-  
+  track.dataset.prevPercentage = track.dataset.percentage; // 얘가 전에 스크롤에서 멈춘 지점을 못읽음.
+  console.log(track.dataset.prevPercentage);
+
 }
 
 const handleOnMove = e => {
@@ -22,17 +23,18 @@ const handleOnMove = e => {
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX
 
   //maxDelta값을 정해줌, window의 width 가 곧 max값
-  const maxDelta = window.innerWidth;
-  //현재 내가 끌어온 거리가 max까지 몇퍼센트인지 
-  const percentage = (mouseDelta / maxDelta) * -100
+  const maxDelta = window.innerWidth; // maxScroll in scroll
   
-  const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage
+  //현재 내가 끌어온 거리가 max까지 몇퍼센트인지 
+  const percentage = (mouseDelta / maxDelta) * -100 // percentage in scroll
+  
+  const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
 
   // nextPercentage가 전체에서 지금까지 몇퍼센트 왔는지 0 부터 -100
   const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
       
   track.dataset.percentage = nextPercentage;
-  
+
   track.animate({
     transform: `translate(${nextPercentage}%, -50%)`
   }, { duration: 1200, fill: "forwards" });
@@ -42,10 +44,32 @@ const handleOnMove = e => {
       objectPosition: `${100 + nextPercentage}% center`
     }, { duration: 1200, fill: "forwards" });
   }
-  // const scrollableElement = document.getElementById("image-track");
-  // const scrollValue = scrollableElement.scroll;
-  // console.log(scrollValue);
+  
 }
+
+
+// //Makes scroll work in horizontally 얘는 전에 마우스가 어디서 멈췄는지 그 퍼센트를 읽어
+// window.addEventListener('wheel', (event) => {
+//   event.preventDefault();
+
+//   window.scrollBy({
+//     left: event.deltaY < 0 ? -20 : 20, 
+    
+//   });
+
+
+//   const maxScroll = document.documentElement.scrollHeight - innerHeight;
+//   const scrolled = parseFloat(window.scrollX);
+//   const scrollPercentage = (scrolled / maxScroll); 
+  
+//   const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + scrollPercentage;
+
+//   // nextPercentage가 전체에서 지금까지 몇퍼센트 왔는지 0 부터 -100
+//   const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+//   console.log(nextPercentage);
+//   track.dataset.prevPercentage = nextPercentage;
+  
+// });
 
 /* -- Had to add extra lines for touch events -- */
 
@@ -60,13 +84,3 @@ window.ontouchend = e => handleOnUp(e.touches[0]);
 window.onmousemove = e => handleOnMove(e);
 
 window.ontouchmove = e => handleOnMove(e.touches[0]);
-
-
-// window.addEventListener('scrollX', () => {
-//   const maxScroll = document.documentElement.scrollHeight - innerHeight;
-//   var scrolled = parseFloat(window.scrollY);
-//   let percentage = scrolled / maxScroll * 100 ;
-
-//   console.log(scrolled);
-//   console.log(`${Math.round(percentage)}%`);
-// })
